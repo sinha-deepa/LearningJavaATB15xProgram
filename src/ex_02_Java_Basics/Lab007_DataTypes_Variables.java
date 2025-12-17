d6d1bee9e8a6c971c0e166e5cb8dc9f6d73b4381
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 21:11:49 +0530

    data types and variables

---
+++ src/ex_02_Java_Basics/Lab007_DataTypes_Variables.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Basics;
+
+public class Lab007_DataTypes_Variables {
+    public static void main (String[] args){
+        boolean flag = true;
+        System.out.println("Flag - "+flag);
+        byte age =9;
+        System.out.println("Age "+age);
+        short admission_number = 127;
+        System.out.println("Admission_number - "+admission_number);
+        int registration_number = 123456789;
+        System.out.println("Registration_number - "+registration_number);
+        long misc = 234l;
+        System.out.println("Misc - "+misc);
+        char section = 'A';
+        System.out.println("Section - "+section);
+        String name = "Deepa";
+        System.out.println("Name - "+name);
+        float marks = 91.5f;
+        System.out.println("Marks - "+marks);
+        double marks_percentage = 99.555555;
+        System.out.println("Marks_percentage - "+marks_percentage);
+    }
+}
