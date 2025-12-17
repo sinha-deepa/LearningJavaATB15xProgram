de2f0847b09cfb225d4305dde47b2c8c0e0954b1
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 18:59:47 +0530

    Table 9 class

---
+++ src/ex_01_Java_Basics/Lab001_Printf_Table9.java
@@ -0,0 +1,26 @@
+package ex_01_Java_Basics;
+
+public class Lab001_Printf_Table9 {
+    public static void main(String[] args) {
+        byte i = 9;
+        System.out.printf("%d x 1 = %d",i, i);
+        System.out.println();
+        System.out.printf("%d x 2 = %d",i, i*2);
+        System.out.println();
+        System.out.printf("%d x 3 = %d",i, i*3);
+        System.out.println();
+        System.out.printf("%d x 4 = %d",i, i*4);
+        System.out.println();
+        System.out.printf("%d x 5 = %d",i, i*5);
+        System.out.println();
+        System.out.printf("%d x 6 = %d",i, i*6);
+        System.out.println();
+        System.out.printf("%d x 7 = %d",i, i*7);
+        System.out.println();
+        System.out.printf("%d x 8 = %d",i, i*8);
+        System.out.println();
+        System.out.printf("%d x 9 = %d",i, i*9);
+        System.out.println();
+        System.out.printf("%d x 10 = %d",i, i*10);
+    }
+}
