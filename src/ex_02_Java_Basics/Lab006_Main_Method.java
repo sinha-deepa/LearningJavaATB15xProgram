012a0a46f4f83622969e0bc7679d9ebbdf737f1c
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 20:47:53 +0530

    main method

---
+++ src/ex_02_Java_Basics/Lab006_Main_Method.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab006_Main_Method {
+    public static void main (String[] args){
+
+    }
+    public static void main (String args) {
+    }
+    public static void Main (String[] args){
+
+    }
+
+}
