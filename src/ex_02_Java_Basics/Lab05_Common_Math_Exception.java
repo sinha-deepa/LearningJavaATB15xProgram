5b20396ebd46eec0da0afb79e68e58fe6fff5fe3
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 20:32:51 +0530

    math exception and operations file

---
+++ src/ex_02_Java_Basics/Lab05_Common_Math_Exception.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab05_Common_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println("2+2");
+        System.out.println(2+2);
+        System.out.println(10/2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(0/2);
+        System.out.println(2+0);
+        System.out.println(2-0);
+        System.out.println(2*0);
+    }
+}
