a4b1ef5918eaac164277e7c86d80a260c33d087e
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 19:52:12 +0530

    types of Comments file

---
+++ src/ex_02_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,30 @@
+package ex_02_Java_Basics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+        System.out.println("Single line comment");
+        // This is single line comment.
+        System.out.println("Single line comment used for multi line comment");
+        // This is single line comment 1.
+        // This is single line comment 2.
+        // This is single line comment 3.
+        // This is single line comment 4.
+        System.out.println("This is documentation");
+        /**
+         * This is multi line comment
+         * This is line number 2
+         * This is line number 3
+         */
+        System.out.println("Multi line comment");
+        /**
+         This is multiple comment
+         *
+         */
+        System.out.println("This is documentation");
+        /*
+        this is documentation comment
+        this is documentation line number 2
+        this is documentation line number 3
+         */
+    }
+}
