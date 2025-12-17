d6ec7d57c7e648ec4e3169d5e2bf94f4843f1277
sinha-deepa <deeps.lata24@gmail.com>
2025-12-17 21:24:23 +0530

    Interview QnA

---
+++ src/ex_02_Java_Basics/Lab008_Interview_QnA1.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab008_Interview_QnA1 {
+    public static void main (String[] args){
+        int _123 = 456;
+       // int _ = 86;
+        byte $ = 23;
+        char char_example_1 = 'Y';
+        long long_name_example_for_lab008_interview_question_and_answer_deepa_kumari=1234567L;
+        System.out.println(_123);
+        //System.out.println(_);
+        System.out.println($);
+        System.out.println(char_example_1);
+        System.out.println(long_name_example_for_lab008_interview_question_and_answer_deepa_kumari);
+    }
+}
